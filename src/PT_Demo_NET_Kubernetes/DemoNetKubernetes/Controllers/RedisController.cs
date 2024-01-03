d9f202ed4a9d692d3deb2b56e70edbd1e22c7948
petartotev <petar@petartotev.net>
2024-01-03 21:33:06 +0200

    Redis demo updated - Read Replicas used, Delete functionality implemented

--- src/PT_Demo_NET_Kubernetes/DemoNetKubernetes/Controllers/RedisController.cs
+++ src/PT_Demo_NET_Kubernetes/DemoNetKubernetes/Controllers/RedisController.cs
@@ -10,37 +10,50 @@ namespace DemoNetKubernetes.Controllers;
 public class RedisController : ControllerBase
 {
     private readonly ILogger<RedisController> _logger;
-    private readonly IDatabase _database;
+    private readonly IDatabase _databaseMaster;
+    private readonly IDatabase _databaseReadReplicas;
 
     public RedisController(ILogger<RedisController> logger)
     {
         _logger = logger;
 
         _logger.LogInformation($"ConnectionMultiplexer.Connect(\"redis-master:6379\") about to be executed with password!");
-
-        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("redis-master:6379,password=usOZHSp51Z");
-        _database = redis.GetDatabase();
-
+        ConnectionMultiplexer redisMaster = ConnectionMultiplexer.Connect("redis-master:6379,password=6t5eH7touz");
+        _databaseMaster = redisMaster.GetDatabase();
         _logger.LogInformation($"ConnectionMultiplexer.Connect(\"redis-master:6379\") successful!");
+
+        _logger.LogInformation($"ConnectionMultiplexer.Connect(\"redis-replicas:6379\") about to be executed with password!");
+        ConnectionMultiplexer redisReadReplicas = ConnectionMultiplexer.Connect("redis-replicas:6379,password=6t5eH7touz");
+        _databaseReadReplicas = redisReadReplicas.GetDatabase();
+        _logger.LogInformation($"ConnectionMultiplexer.Connect(\"redis-replicas:6379\") successful!");
     }
 
     [HttpPost(Name = "SetRedisValueByKey")]
     [Route("set")]
     public ObjectResult Set(RedisEntity obj)
     {
-        if (_database.StringSet(obj.Key, obj.Value)) return Ok($"{obj.Key}:{obj.Value} stored in Redis!");
+        if (_databaseMaster.StringSet(obj.Key, obj.Value)) return Ok($"{obj.Key}:{obj.Value} stored in Redis!");
 
-        return BadRequest($"Redis did not manage to store {obj.Key}:{obj.Value}!");
+        return BadRequest($"Redis failed to store {obj.Key}:{obj.Value}!");
     }
 
-    [HttpGet(Name = "GetRedisKeyByValue")]
+    [HttpGet(Name = "GetRedisValueByKey")]
     [Route("get/{key}")]
     public async Task<ObjectResult> GetAsync(string key)
     {
-        var value = await _database.StringGetAsync(key);
+        var value = await _databaseReadReplicas.StringGetAsync(key);
 
         if (!value.IsNull && value.HasValue) return Ok(value.ToString());
 
-        return NotFound($"Redis value was not found by key {key}!");
+        return NotFound($"Redis key `{key}` not found!");
+    }
+
+    [HttpDelete(Name = "DeleteRedisKey")]
+    [Route("delete/{key}")]
+    public async Task<ObjectResult> DeleteAsync(string key)
+    {
+        var isDeleted = await _databaseMaster.KeyDeleteAsync(key);
+
+        return isDeleted ? Ok($"Redis key `{key}` deleted!") : NotFound($"Redis key `{key}` not found!");
     }
 }
